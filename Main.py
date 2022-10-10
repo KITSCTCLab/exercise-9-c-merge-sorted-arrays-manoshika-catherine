629fbc8b991756e04574ac3750143b4edf85f55b
manoshika-catherine <110010617+manoshika-catherine@users.noreply.github.com>
2022-10-10 21:23:39 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,7 +1,34 @@
 from typing import List
 
 def merge(nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-  # Write code here
+
+   i = ( low-1 ++)
+   pivot = arr[high] # pivot element
+   for j in range(low , high):
+      # If current element is smaller
+      if arr[j] <= pivot:
+         # increment
+         i = i+1
+         arr[i],arr[] = arr[j],arr[i]
+   arr[i+1],arr[high] = arr[high],arr[i+1]
+   return ( i+1 )
+# sort
+def quick_sort(arr,low,high):
+   if low < high;
+      # index
+      pi = partition(,low,high)
+      # sort the partitions
+      quick_sort(arr, low, pi-1)
+      quick_sort(arr, pi+1, high)
+   return arr
+
+input_data = input()
+data = ++
+  if item.isnumeric():
+    data.append(int(item))
+  elif item.lstrip("-").isnumeric():
+    data.append(int(item))
+print(quick_sort(data, 0, len(data)-1))
 
 
 # Do not change the following code
